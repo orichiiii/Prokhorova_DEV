1923076f1083c262438ef177c0554008e6993c82
olya prohorova <prohorova0695@gmail.com>
2021-05-26 14:01:25 +0300

    delete and update data in db

--- SQL/UnitTest1.cs
+++ SQL/UnitTest1.cs
@@ -1,18 +1,62 @@
+using System.Collections.Generic;
 using NUnit.Framework;
 
-namespace SQL
+namespace SQL.Tests
 {
     public class Tests
     {
+        private SqlHelper _sqlHelper;
+
         [SetUp]
         public void Setup()
         {
+            _sqlHelper = new SqlHelper("Shop");
+            _sqlHelper.OpenConnection();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _sqlHelper.ExecuteNonQuery("delete from [Shop].[dbo].[Products] where id = 23");
+            _sqlHelper.CloseConnection();
         }
 
         [Test]
-        public void Test1()
+        public void AddProduct()
         {
-            Assert.Pass();
+            _sqlHelper.Insert("Products",
+                new Dictionary<string, string> { { "Id", "23" }, { "Name", "'Test23'" }, { "Count", "234" } });
+            var res = _sqlHelper.IsRowExistedInTable("Products",
+                new Dictionary<string, string> { { "Id", "23" }, { "Name", "'Test23'" }, { "Count", "234" } });
+
+            Assert.True(res);
+        }
+
+        [Test]
+        public void UpdateData()
+        {
+            _sqlHelper.Insert("Products",
+                new Dictionary<string, string> { { "Id", "23" }, { "Name", "'Test23'" }, { "Count", "234" } });
+            _sqlHelper.Update("Products",
+                new Dictionary<string, string> { { "Id", "30" }, { "Name", "'Apple'" }, { "Count", "2000" } }, new Dictionary<string, string> {{ "Name", "'Test23'" } });
+            var res = _sqlHelper.IsRowExistedInTable("Products",
+                new Dictionary<string, string> { { "Id", "30" }, { "Name", "'Apple'" }, { "Count", "2000" } });
+
+            Assert.True(res);
+        }
+
+        [Test]
+        public void DeleteData()
+        {
+            _sqlHelper.Insert("Products",
+                new Dictionary<string, string> { { "Id", "23" }, { "Name", "'Test23'" }, { "Count", "234" } });
+
+            _sqlHelper.DeleteProduct("Products", new Dictionary<string, string> { { "Id", "23" }, { "Name", "'Test23'" }, { "Count", "234" } });
+            
+            var res = _sqlHelper.IsRowExistedInTable("Products",
+                new Dictionary<string, string> { { "Id", "23" }, { "Name", "'Test23'" }, { "Count", "234" } });
+
+            Assert.False(res);
         }
     }
 }
